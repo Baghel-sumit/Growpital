744962007b9d9c08c3157a949b2b1d969c287402
SUMIT BAGHEL <94341777+Sumit123bahgel@users.noreply.github.com>
2022-09-16 19:38:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,7 @@ const btn = document.querySelector('.target');
 const main = document.querySelector('main');
 const pop = document.querySelector('.popup');
 const cross = document.querySelector('.cross'); 
-main.style.filter = 'blur(10px)';
-if(pop.classList.contains('active')){
-    main.style.filter = 'blur(10px)';
-}
+
 btn.addEventListener('click',()=>{
     pop.classList.add('active');
     main.style.filter = 'blur(10px)';
